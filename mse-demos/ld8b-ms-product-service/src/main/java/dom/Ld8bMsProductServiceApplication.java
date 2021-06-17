df6caf8f33803e3fecb3bff637768e821361dd58
livegit2 <livegit2@gmail.com>
2021-06-17 11:40:54 +0530

    commit

---
+++ mse-demos/ld8b-ms-product-service/src/main/java/dom/Ld8bMsProductServiceApplication.java
@@ -0,0 +1,13 @@
+package dom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ld8bMsProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ld8bMsProductServiceApplication.class, args);
+	}
+
+}
